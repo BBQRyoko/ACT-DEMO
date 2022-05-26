c95693f56aec1ceade98e87b8b93946f8f5de9fa
YinqiaoChen <yc636@scarletmail.rutgers.edu>
2022-05-26 04:00:07 -0400

    Working on tornado defelect effect now

--- Assets/Scripts/TornadoHazard.cs
+++ Assets/Scripts/TornadoHazard.cs
@@ -1,9 +1,19 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TornadoHazard : MonoBehaviour
 {
+    FlyingObj reflectFlyingObj;
+    [SerializeField] Transform shootPos;
+
+    //可以反弹攻击
+    void DefelectFlyingObj(FlyingObj obj) 
+    {
+    
+    }
+
+    //可以吸收火焰
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("DarkKnight"))
@@ -11,6 +21,12 @@ public class TornadoHazard : MonoBehaviour
             Debug.Log("123");
             other.GetComponentInChildren<EnemyAnimatorManager>().tornadoSlashEnhance = true;
             Destroy(transform.gameObject);
+        }
+
+        if (other.GetComponentInParent<FlyingObj>()) 
+        {
+            DefelectFlyingObj(other.GetComponentInParent<FlyingObj>());
+            Destroy(other.gameObject);
         }
     }
 }
