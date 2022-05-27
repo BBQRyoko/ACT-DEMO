12e06a3f38324161dd6c52899d323970ab49c49d
YinqiaoChen <yc636@scarletmail.rutgers.edu>
2022-05-27 02:20:38 -0400

    some updates

--- Assets/Scripts/TornadoHazard.cs
+++ Assets/Scripts/TornadoHazard.cs
@@ -31,24 +31,24 @@ public class TornadoHazard : MonoBehaviour
     //可以反弹攻击
     void DefelectFlyingObj(FlyingObj obj) 
     {
-        if (!deflectFlyingObj) 
-        {
-            deflectFlyingObj = obj;
-            var defelectObj = Instantiate(deflectFlyingObj, shootPos, false);
-            defelectObj.transform.SetParent(null);
-            defelectObj.gameObject.SetActive(true);
-            defelectObj.StartFlyingObj(deflectFlyingObj.shooterPos, false, deflectFlyingObj.shooterPos, true);
-            ProjectileDamager projectileDamager = defelectObj.GetComponentInChildren<ProjectileDamager>();
-            if (projectileDamager.isPlayerDamage)
-            {
-                projectileDamager.isPlayerDamage = false;
-            }
-            else
-            {
-                projectileDamager.isPlayerDamage = true;
-            }
-            deflectFlyingObj = null;
-        }
+        //if (!deflectFlyingObj) 
+        //{
+        //    deflectFlyingObj = obj;
+        //    var defelectObj = Instantiate(deflectFlyingObj, shootPos, false);
+        //    defelectObj.transform.SetParent(null);
+        //    defelectObj.gameObject.SetActive(true);
+        //    defelectObj.StartFlyingObj(deflectFlyingObj.shooterPos, false, deflectFlyingObj.shooterPos, true);
+        //    ProjectileDamager projectileDamager = defelectObj.GetComponentInChildren<ProjectileDamager>();
+        //    if (projectileDamager.isPlayerDamage)
+        //    {
+        //        projectileDamager.isPlayerDamage = false;
+        //    }
+        //    else
+        //    {
+        //        projectileDamager.isPlayerDamage = true;
+        //    }
+        //    deflectFlyingObj = null;
+        //}
        
         //获得释放者的位置信息，然后以那个为目标发射
         //飞行道具本身生成时检测是否有playerManager或者enemyManager
